80ccbcaf7a0058ae10ae85060bf91accb62cb7ff
shammaskhan <shammaskhan365@gmai.com>
2026-06-29 20:24:23 +0530

    Move registration to POM

--- pages/ParaBankPage.ts
+++ pages/ParaBankPage.ts
@@ -9,4 +9,20 @@ export class ParaBankPage {
             'https://parabank.parasoft.com/parabank/index.htm?ConnType=JDBC'
         );
     }
+    async registerUser(username: string, password: string) {
+         await this.page.getByRole('link', { name: 'Register' }).click();
+        await this.page.locator('[id="customer.firstName"]').fill('shammas');
+        await this.page.locator('[id="customer.lastName"]').fill('khan');
+        await this.page.locator('[id="customer.address.street"]').fill('Andheri East');
+        await this.page.locator('[id="customer.address.city"]').fill('Mumbai');
+        await this.page.locator('[id="customer.address.state"]').fill('Maharashtra');
+        await this.page.locator('[id="customer.address.zipCode"]').fill('400069');
+        await this.page.locator('[id="customer.phoneNumber"]').fill('9404451573');
+        await this.page.locator('[id="customer.ssn"]').fill('ABC');
+        await this.page.locator('[id="customer.username"]').fill(username);
+        await this.page.locator('[id="customer.password"]').fill(password);
+        await this.page.locator('#repeatedPassword').fill(password);
+        await this.page.getByRole('button', { name: 'Register' }).click();
+
+}
 }
--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -13,20 +13,9 @@ test('Register a new user and verify account balance', async ({ page }) => {
     await test.step('Starting Registration ', async () => {
         const username = `shams${Date.now()}`;
         const password = 'qwerty';
-        await page.getByRole('link', { name: 'Register' }).click();
-        await page.locator('[id="customer.firstName"]').fill('shammas');
-        await page.locator('[id="customer.lastName"]').fill('khan');
-        await page.locator('[id="customer.address.street"]').fill('Andheri East');
-        await page.locator('[id="customer.address.city"]').fill('Mumbai');
-        await page.locator('[id="customer.address.state"]').fill('Maharashtra');
-        await page.locator('[id="customer.address.zipCode"]').fill('400069');
-        await page.locator('[id="customer.phoneNumber"]').fill('9404451573');
-        await page.locator('[id="customer.ssn"]').fill('ABC');
-        await page.locator('[id="customer.username"]').fill(username);
-        await page.locator('[id="customer.password"]').fill(password);
-        await page.locator('#repeatedPassword').fill(password);
-        await page.getByRole('button', { name: 'Register' }).click();
-        });
+        await test.step('Starting Registration', async () => {
+        await paraBank.registerUser(username, password);
+    });
         await test.step('Verify the Number displayed on Screen', async () => {
         await page.getByRole('link', { name: 'Accounts Overview' }).click();
         await page.locator('b').filter({ hasText: '$' }).click();
@@ -34,4 +23,5 @@ test('Register a new user and verify account balance', async ({ page }) => {
         console.log('Total Balance is displayed on the screen = ' + await page.locator('tbody b').nth(1).textContent());
         });
     
+});
 });
